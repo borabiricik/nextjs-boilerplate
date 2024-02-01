40166b4a235a0c8670cab36f1a8b95d535cd046c
borabiricik <bora.bir496@gmail.com>
2024-02-01 17:22:54 +0300

    feat: husky added

--- src/app/auth/login/actions/login.action.ts
+++ src/app/auth/login/actions/login.action.ts
@@ -1,17 +1,17 @@
-'use server';
-import instance from '@/app/core/lib/axios';
-import { cookies } from 'next/headers';
+"use server"
+import instance from "@/services/axios"
+import { cookies } from "next/headers"
 
 export const loginAction = async (values: any) => {
   try {
-    const { data } = await instance.post('/auth/login', values);
-    const cookieStore = cookies();
-    cookieStore.set('token', data.access_token, {
+    const { data } = await instance.post("/auth/login", values)
+    const cookieStore = cookies()
+    cookieStore.set("token", data.access_token, {
       httpOnly: true,
       secure: true,
-    });
-    return data;
+    })
+    return data
   } catch (error: any) {
-    return error.response.data;
+    return error.response.data
   }
-};
+}
