ae8fdce25ff3b334da9c45d40a14b0e27693dead
borabiricik <bora.bir496@gmail.com>
2024-02-01 17:59:34 +0300

    feat: vscode config & dummy component

--- src/app/common/components/Button/index.tsx
+++ src/app/common/components/Button/index.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image"
+
 const Button = () => {
-  return <div>Button</div>
+  return (
+    <div>
+      <Image
+        src={"https://placehold.co/400"}
+        alt="dummy image"
+        width={400}
+        height={400}
+      />
+    </div>
+  )
 }
 
 export default Button
